dcc6d6421d6ccf248a6face68892d0b4c7a4f1b2
DashginAsgarli <dashqinasgarli@gmail.com>
2026-03-04 23:27:47 +0400

    feat: implement LiveKitchen section and layout

--- src/page/Hero.jsx
+++ src/page/Hero.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import HomePage from '../components/HomePage'
 import DealsSection from '../components/DealsSection'
 import MobileApp from '../components/MobileApp'
+import LiveKitchen from '../components/LiveKitchen'
 
 function Hero() {
     return (
         <>
             <HomePage />
             <DealsSection />
+
+            <LiveKitchen />
             <MobileApp />
         </>
     )
